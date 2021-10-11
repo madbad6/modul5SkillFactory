8080465a18c32fc539fa8f7f36e4ea0d8f293312
Nicolay Elsukov <madbad6@yandex.ru>
2021-10-11 17:09:33 +0300

    the second commit "hallow Friend"

--- src/com/madbad/Main.java
+++ src/com/madbad/Main.java
@@ -3,6 +3,6 @@ package com.madbad;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hallow Word");
+        System.out.println("Hallow Friend");
     }
 }
